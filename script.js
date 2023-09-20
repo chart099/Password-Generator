3c8796b387909a081a90e3b1a88494d026f07808
chart099 <122659980+chart099@users.noreply.github.com>
2023-09-20 17:07:51 -0500

    worked out how to select different types of characters

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var passLength = prompt("How many characters do you want in your password?")
+
+var charOptions = "";
+var newPassword = "";
+
+if (passLength < 8 || passLength > 128) {
+  alert("Sorry, we can only make passwords between 8 and 128 characters.")
+} else {
+  if (confirm("Do you want to include uppercase letter?")) {
+    charOptions += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } 
+  
+  if (confirm("Do you want to include lowercase letters?")) {
+    charOptions += "abcdefghijklmnopqrstuvwxyz"
+  }
+  
+  if (confirm("Do you want to include numbers?")) {
+    charOptions += "1234567890";
+  }
+
+  if (confirm("Do you want to indclude special characters?")) {
+    charOptions += "!@#$%^&*()"
+  } 
+
+  if (charOptions.length === 0) {
+    alert("Error, you must pick at least one option")
+  }
+
+  for (let i = 0; i < passLength - 1; i++) {
+    newPassword += charOptions[(Math.floor(Math.random()*charOptions.length))];
+  }
+
+  console.log(newPassword);
+
+}
+   
