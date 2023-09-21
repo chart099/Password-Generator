f4c55db25d941cd8d719b45322757fe129159329
chart099 <122659980+chart099@users.noreply.github.com>
2023-09-20 19:40:46 -0500

    removed unnecessary line

--- script.js
+++ script.js
@@ -6,30 +6,32 @@ function writePassword() {
   var newPassword = "";
 
   function generatePassword () {
+    // opening message
     var passLength = prompt("How many characters do you want in your password?")
-        
+    // space to hold character options
     var charOptions = "";
     
-    
+    // make sure the characters are inside the limits
     if (passLength < 8 || passLength > 128) {
       alert("Sorry, we can only make passwords between 8 and 128 characters.")
     } else {
+      // put in uppercase letter
       if (confirm("Do you want to include uppercase letter?")) {
         charOptions += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       } 
-      
+      // put in lowercase letters
       if (confirm("Do you want to include lowercase letters?")) {
         charOptions += "abcdefghijklmnopqrstuvwxyz"
       }
-      
+      // put in numbers
       if (confirm("Do you want to include numbers?")) {
         charOptions += "1234567890";
       }
-    
+      // put in special characters
       if (confirm("Do you want to indclude special characters?")) {
         charOptions += "!@#$%^&*()"
       } 
-    
+      // alert if no options are selected
       if (charOptions.length === 0) {
         alert("Error, you must pick at least one option")
       }
@@ -37,11 +39,7 @@ function writePassword() {
       for (let i = 0; i < passLength - 1; i++) {
         newPassword += charOptions[(Math.floor(Math.random()*charOptions.length))];
       }
-    
-      console.log(newPassword);
-    
     }
-    
     }
 
 
