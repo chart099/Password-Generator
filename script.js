670df46a19ac8240e0714f56845763d46fba6c29
chart099 <122659980+chart099@users.noreply.github.com>
2023-09-20 19:31:56 -0500

    got the password to appear in the box

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-
-function generatePassword () {
-var passLength = prompt("How many characters do you want in your password?")
-    
-var charOptions = "";
-var newPassword = "";
-
-if (passLength < 8 || passLength > 128) {
-  alert("Sorry, we can only make passwords between 8 and 128 characters.")
-} else {
-  if (confirm("Do you want to include uppercase letter?")) {
-    charOptions += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } 
-  
-  if (confirm("Do you want to include lowercase letters?")) {
-    charOptions += "abcdefghijklmnopqrstuvwxyz"
-  }
-  
-  if (confirm("Do you want to include numbers?")) {
-    charOptions += "1234567890";
-  }
-
-  if (confirm("Do you want to indclude special characters?")) {
-    charOptions += "!@#$%^&*()"
-  } 
-
-  if (charOptions.length === 0) {
-    alert("Error, you must pick at least one option")
-  }
-
-  for (let i = 0; i < passLength - 1; i++) {
-    newPassword += charOptions[(Math.floor(Math.random()*charOptions.length))];
-  }
-
-  console.log(newPassword);
-
-}
-
-}
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var newPassword = "";
+
+  function generatePassword () {
+    var passLength = prompt("How many characters do you want in your password?")
+        
+    var charOptions = "";
+    
+    
+    if (passLength < 8 || passLength > 128) {
+      alert("Sorry, we can only make passwords between 8 and 128 characters.")
+    } else {
+      if (confirm("Do you want to include uppercase letter?")) {
+        charOptions += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      } 
+      
+      if (confirm("Do you want to include lowercase letters?")) {
+        charOptions += "abcdefghijklmnopqrstuvwxyz"
+      }
+      
+      if (confirm("Do you want to include numbers?")) {
+        charOptions += "1234567890";
+      }
+    
+      if (confirm("Do you want to indclude special characters?")) {
+        charOptions += "!@#$%^&*()"
+      } 
+    
+      if (charOptions.length === 0) {
+        alert("Error, you must pick at least one option")
+      }
+    
+      for (let i = 0; i < passLength - 1; i++) {
+        newPassword += charOptions[(Math.floor(Math.random()*charOptions.length))];
+      }
+    
+      console.log(newPassword);
+    
+    }
+    
+    }
 
 
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = newPassword;
 
 }
 
